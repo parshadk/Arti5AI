331a569ff409eb64e58ba6da074ecb32a2bebbf3
parshadk <parshadk04@gmail.com>
2024-09-14 03:32:42 +0530

    Fixed Bugs

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-6xl w-full space-y-8">
         <div className="md:flex md:space-x-8">
-          <div className="md:w-1/2 bg-white p-10 rounded-xl shadow-md mb-8 md:mb-0 bg-gradient-to-br from-blue-50 to-indigo-100">
+          <div className="md:w-1/2 bg-white p-10 rounded-xl shadow-md mb-8 md:mb-0 bg-gradient-to-br from-indigo-100 to-blue-200">
             <div className="text-center md:text-left">
               <h1 className="text-4xl font-bold text-gray-900 bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">
                 Hello, I am Parshad Keni
@@ -33,15 +33,11 @@ export default function AboutMe() {
                 <span className="sr-only">Twitter</span>
                 <Twitter className="h-6 w-6" />
               </a>
-              <a href="https://www.instagram.com/parshad1694?igsh=MThucWtlcHh1bXJycg==" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900 transition-colors duration-300">
-                <span className="sr-only">Instagram</span>
-                <Instagram className="h-6 w-6" />
-              </a>
             </div>
 
             <div className="mt-8 flex justify-center md:justify-start">
               <a
-                href="https://drive.google.com/file/d/1HOpx-w7AdTnaHKEgXYQsclb_p6zgAFxV/view?usp=sharing"
+                href="https://drive.google.com/file/d/1I8HJBPq1ZgNAeIsiz4ZyLNrB4vV_1bDV/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 transition-colors duration-300"
@@ -51,18 +47,20 @@ export default function AboutMe() {
             </div>
           </div>
 
-          <div className="md:w-1/2 bg-white p-10 rounded-xl shadow-md bg-gradient-to-br from-purple-50 to-pink-100">
-            <h2 className="text-2xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-pink-600">Featured Project: Arti5AI</h2>
+          <div className="md:w-1/2 bg-white p-10 rounded-xl shadow-md bg-gradient-to-br from-indigo-100 to-blue-200">
+            <h2 className="text-2xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-pink-600">About this Project: Arti5AI</h2>
             <p className="text-gray-700 mb-4">
-              Developed an AI-powered content generation app using Next.js, React, Tailwind CSS, TypeScript, and Google Gemini API. Focused on creating user-friendly templates and generating AI-driven content.
+              AI-powered content generation app  developed using Next.js, React, Tailwind CSS, TypeScript, and Google Gemini API. Focused on creating user-friendly templates and generating AI-driven content.
             </p>
             <ul className="list-disc list-inside text-gray-700 mb-4">
               <li>Authentication: Implemented secure social and email/password authentication using Clerk.</li>
               <li>Database Integration: Set up Drizzle ORM and PostgreSQL to manage and store data efficiently.</li>
               <li>AI Content Generation: Integrated Google Gemini API to create customized AI-generated content.</li>
+              <li>Customizable Content : Integrated Toast UI Rich Text Editor to edit the generated content.</li>
+              <li>Payment Integration: Used RazorPay to integrate payment functionality.</li>
             </ul>
             <p className="text-gray-800 font-semibold">
-              Tech Stack: Next.js, React, Tailwind CSS, TypeScript, Google Gemini API, Clerk, Drizzle ORM, PostgreSQL.
+              Tech Stack: Next.js, React, Tailwind CSS, TypeScript, Google Gemini API, Clerk, Drizzle ORM, PostgreSQL ,RazorPay.
             </p>
           </div>
         </div>
