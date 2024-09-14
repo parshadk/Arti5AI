e40226f364198c75d14798aa041b574da9a4844f
Parshad <138670180+parshadk@users.noreply.github.com>
2024-09-14 16:53:56 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center bg-blue-300">
+    <div className="flex items-center justify-center bg-blue-200">
       <SignIn />
     </div>
   );
