6d8c0a294208b547c8e2de822b8b1129489fb3e4
Parshad <138670180+parshadk@users.noreply.github.com>
2024-09-14 16:54:16 +0530

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center bg-blue-300">
+    <div className="flex items-center justify-center bg-blue-200">
       <SignUp />
     </div>
   );
